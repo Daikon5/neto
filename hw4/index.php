694ec5294e3c194598c63233dbb804899d933613
Daikon5 <fiveman123@gmail.com>
2017-12-02 21:05:08 +0600

    Update index.php

--- hw4/index.php
+++ hw4/index.php
@@ -1,5 +1,26 @@
 <?php
-$site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=Omsk,ru&appid=b1b15e88fa797225412429c1c50c122a1");
+$city = "Omsk";
+$appid = "b1b15e88fa797225412429c1c50c122a1";
+$filename = "json.txt";
+
+if (file_exists($filename)) {
+    if ((time() - touch($filename)) > 3600) {
+        $handle = fopen($filename,"w");
+        $site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=".$city.",ru&appid=".$appid);
+        fwrite($handle,$site);
+        fclose($handle);
+    }
+    else {
+        $site = file_get_contents($filename);
+    }
+}
+else {
+    $handle = fopen($file,"w");
+    $site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=".$city.",ru&appid=".$appid);
+    fwrite($handle,$site);
+    fclose($handle);
+}
+
 $result = json_decode($site, true);
 echo "<pre>";
 echo "City: " . $result["name"]. "\n";

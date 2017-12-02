e2b760e0a257fd62271b628efcaa0710c3896728
Daikon5 <fiveman123@gmail.com>
2017-12-02 21:15:08 +0600

    Update index.php

--- hw4/index.php
+++ hw4/index.php
@@ -4,7 +4,7 @@ $appid = "b1b15e88fa797225412429c1c50c122a1";
 $filename = "json.txt";
 
 if (file_exists($filename)) {
-    if ((time() - touch($filename)) > 3600) {
+    if ((time() - filemtime($filename)) > 3600) {
         $handle = fopen($filename,"w");
         $site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=".$city.",ru&appid=".$appid);
         fwrite($handle,$site);

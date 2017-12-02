39f32c6977c8d3734934d9ecdca6efafec15c30d
Daikon5 <fiveman123@gmail.com>
2017-12-02 21:23:03 +0600

    Update index.php

--- hw4/index.php
+++ hw4/index.php
@@ -3,19 +3,11 @@ $city = "Omsk";
 $appid = "b1b15e88fa797225412429c1c50c122a1";
 $filename = "json.txt";
 
-if (file_exists($filename)) {
-    if ((time() - filemtime($filename)) > 3600) {
-        $handle = fopen($filename,"w");
-        $site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=".$city.",ru&appid=".$appid);
-        fwrite($handle,$site);
-        fclose($handle);
-    }
-    else {
-        $site = file_get_contents($filename);
-    }
+if (file_exists($filename) && ((time() - filemtime($filename)) < 3600)) {
+    $site = file_get_contents($filename);
 }
 else {
-    $handle = fopen($file,"w");
+    $handle = fopen($filename,"w");
     $site = file_get_contents("http://openweathermap.org/data/2.5/weather?q=".$city.",ru&appid=".$appid);
     fwrite($handle,$site);
     fclose($handle);
@@ -25,4 +17,4 @@ $result = json_decode($site, true);
 echo "<pre>";
 echo "City: " . $result["name"]. "\n";
 echo "Weather: " . $result["weather"]["0"]["main"] . "\n";
-echo "Temperature: " . $result["main"]["temp"];
+echo "Temperature: " . $result["main"]["temp"] . "\n";

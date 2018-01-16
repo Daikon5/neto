eed7a357c56ae1503f4e79fece4176df7cd8d383
Daikon5 <fiveman123@gmail.com>
2018-01-16 23:27:59 +0600

    1

---
+++ hw13/index.php
@@ -0,0 +1,80 @@
+<?php
+$sqlCommand = "SELECT * FROM tasks";
+$sqlAdd = NULL;
+$sqlDone = NULL;
+$sqlDel = NULL;
+if (!empty($_POST)) {
+    if (isset($_POST['task_add'])) {
+        $task = $_POST['task_add'];
+        $taskDate = date('D.M.Y')." ".date('H:i:s');
+        $sqlAdd = "INSERT INTO tasks (id, description, is_done, date_added) VALUES (?,?,?,?)";
+    }
+    elseif (isset($_POST['task_done'])) {
+        $taskDone = (int)$_POST['task_done'];
+        $sqlDone = "UPDATE tasks WHERE id=? SET is_done=1";
+    }
+    elseif (isset($_POST['task_del'])) {
+        $taskDel = (int)$_POST['task_del'];
+        $sqlDel = "DELETE FROM tasks WHERE id =?";
+    }
+    else {
+        echo "Whoops!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="" rel="stylesheet" type="text/css">
+    <title>ToDo</title>
+</head>
+<body>
+<h1>ToDo:</h1>
+
+<?php
+$db = new PDO("mysql:host=localhost;dbname=netology;charset=utf8",'mysql','mysql');
+
+if ($sqlAdd != NULL) {
+    $sth = $db->prepare($sqlAdd);
+    $sth->execute([NULL,$task,0,$taskDate]) or die ("Problems adding!");
+}
+if ($sqlDone != NULL) {
+    $sth = $db->prepare($sqlDone);
+    $sth->execute([$taskDone]) or die ("Problems checking!");
+}
+if ($sqlDel != NULL) {
+    $sth = $db->prepare($sqlDel) or die ("Problems deleting!");
+    $sth->execute([$taskDel]);
+}
+
+$query = $db->query($sqlCommand) or die ("Query problems, pal!");
+echo "<table border='1'>";
+echo "<tr><th>id</th><th>Задача</th><th>Выполнена?</th><th>Дата создания</th></tr>";
+while ($tablerows = $query->fetch()) {
+    echo "<tr><td>$tablerows[0]</td><td>$tablerows[1]</td><td>$tablerows[2]</td><td>$tablerows[3]</td></tr>";
+}
+echo "</table>";
+?>
+
+<form method="POST">
+    <p>Добавить задачу:</p>
+    <input type="text" name="task_add" placeholder="Описание задачи">
+    <input type="submit" value="Добавить">
+</form>
+
+<form method="POST">
+    <p>Отметить задачу как выполненную:</p>
+    <input type="text" name="task_done" placeholder="id задачи">
+    <input type="submit" value="Отметить">
+</form>
+
+<form method="POST">
+    <p>Удалить задачу:</p>
+    <input type="text" name="task_del" placeholder="id задачи">
+    <input type="submit" value="Удалить">
+</form>
+
+</body>
+</html>

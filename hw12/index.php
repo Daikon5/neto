ad8045983c0d06a06fdc14038ae28b3ee968ecde
Daikon5 <fiveman123@gmail.com>
2018-01-18 17:58:22 +0600

    Update index.php

--- hw12/index.php
+++ hw12/index.php
@@ -9,43 +9,51 @@ if (!empty($_GET)) {
     $sqlCommand .= " WHERE";
     $i = count($_GET);
     foreach ($_GET as $key => $item) {
+        $item = strip_tags($item);
         $sqlCommand .= " $key LIKE '%$item%'";
         if ($c < $i) {
             $sqlCommand .= " AND";
         }
         $c++;
     }
+    echo $sqlCommand;
 }
 ?>
 
-    <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <link href="" rel="stylesheet" type="text/css">
-        <title>Каталог</title>
-    </head>
-    <body>
-    <h1>Каталог:</h1>
-    <form method="get">
-        <input type="text" name="isbn" placeholder="ISBN" value="<?php if (isset($_GET['isbn'])) {echo $_GET['isbn'];} ?>">
-        <input type="text" name="name" placeholder="Название книги" value="<?php if (isset($_GET['name'])) {echo $_GET['name'];} ?>">
-        <input type="text" name="author" placeholder="Автор книги" value="<?php if (isset($_GET['author'])) {echo $_GET['author'];} ?>">
-        <input type="submit" value="Поиск">
-    </form><br><br>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="" rel="stylesheet" type="text/css">
+    <title>Каталог</title>
+</head>
+<body>
+<h1>Каталог:</h1>
+<form method="get">
+    <input type="text" name="isbn" placeholder="ISBN" value="<?php if (isset($_GET['isbn'])) {echo $_GET['isbn'];} ?>">
+    <input type="text" name="name" placeholder="Название книги" value="<?php if (isset($_GET['name'])) {echo $_GET['name'];} ?>">
+    <input type="text" name="author" placeholder="Автор книги" value="<?php if (isset($_GET['author'])) {echo $_GET['author'];} ?>">
+    <input type="submit" value="Поиск">
+</form><br><br>
 
 <?php
-$db = new mysqli('localhost','mysql','mysql','netology');
-$db->set_charset('utf-8');
-$query = $db->query($sqlCommand,MYSQLI_USE_RESULT) or die ("Query problems, pal!");
+try {
+    $db = new PDO("mysql:host=localhost;dbname=netology;charset=utf8", 'mysql', 'mysql');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $query = $db->prepare($sqlCommand);
+    $query->execute();
+}
+catch (PDOException $e) {
+    $e->getMessage();
+}
 
 echo "<table border='1'>";
 echo "<tr><th>Название</th><th>Автор</th><th>Год выпуска</th><th>Жанр</th><th>ISBN</th></tr>";
-while ($tablerows = $query->fetch_row()) {
+while ($tablerows = $query->fetch()) {
     echo "<tr><td>$tablerows[1]</td><td>$tablerows[2]</td><td>$tablerows[3]</td><td>$tablerows[5]</td><td>$tablerows[4]</td></tr>";
 }
 echo "</table>";
 ?>
 
-    </body>
-    </html>
+</body>
+</html>
